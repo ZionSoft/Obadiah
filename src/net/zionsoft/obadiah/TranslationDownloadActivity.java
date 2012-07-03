cf5eec364831e1c8f0728b26b69a70fe2909ab5a
Xizhi Zhu <xizhi.zhu@gmail.com>
2012-07-03 20:58:11 +0300

    Refactored to use AsyncTask for downloading.

--- src/net/zionsoft/obadiah/TranslationDownloadActivity.java
+++ src/net/zionsoft/obadiah/TranslationDownloadActivity.java
@@ -1,10 +1,22 @@
 package net.zionsoft.obadiah;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 import android.app.Activity;
 import android.app.ProgressDialog;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Message;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
@@ -30,67 +42,168 @@ public class TranslationDownloadActivity extends Activity
         {
             public void onItemClick(AdapterView<?> parent, View view, int position, long id)
             {
-                m_progressDialog = new ProgressDialog(TranslationDownloadActivity.this);
-                m_progressDialog.setCancelable(false);
-                m_progressDialog.setMessage(getText(R.string.text_downloading));
-                m_progressDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
-                m_progressDialog.setMax(100);
-                m_progressDialog.setProgress(0);
-                m_progressDialog.show();
-
-                Handler handler = m_downloadManager.handler();
-                Message message = Message.obtain(handler, DownloadManager.MESSAGE_DOWNLOAD_TRANSLATION);
-                message.arg1 = position;
-                handler.sendMessage(message);
+                new TranslationDownloadAsyncTask().execute(position);
             }
         });
 
-        Handler handler = new Handler()
-        {
-            public void handleMessage(Message message)
-            {
-                switch (message.what) {
-                case MESSAGE_TRANSLATIONLIST_DOWNLOADED: {
-                    TranslationInfo[] availableTranslations = m_downloadManager.availableTranslations();
-                    if (availableTranslations == null || availableTranslations.length == 0) {
-                        m_progressDialog.dismiss();
-                        Toast.makeText(TranslationDownloadActivity.this, R.string.text_no_available_translation,
-                                Toast.LENGTH_SHORT).show();
-                        finish();
-                    } else {
-                        int length = availableTranslations.length;
-                        String[] texts = new String[length];
-                        for (int i = 0; i < length; ++i)
-                            texts[i] = availableTranslations[i].name + " (" + availableTranslations[i].size + " KB)";
-                        m_adapter.setTexts(texts);
-                        m_progressDialog.dismiss();
-                    }
-                    break;
-                }
-                case MESSAGE_TRANSLATION_DOWNLOADED: {
-                    BibleReader.getInstance().refresh();
-                    m_progressDialog.dismiss();
-                    finish();
-                    break;
-                }
-                case MESSAGE_DOWNLOAD_PROGRESS: {
-                    m_progressDialog.setProgress(message.arg1);
-                    break;
-                }
-                }
-            }
-        };
-
-        m_progressDialog = new ProgressDialog(this);
-        m_progressDialog.setCancelable(false);
-        m_progressDialog.setMessage(getText(R.string.text_downloading));
-        m_progressDialog.show();
-
-        m_downloadManager = new DownloadManager(handler, this);
-        m_downloadManager.start();
+        new TranslationListDownloadAsyncTask().execute();
     }
 
-    private DownloadManager m_downloadManager;
-    private ProgressDialog m_progressDialog;
+    private class TranslationListDownloadAsyncTask extends AsyncTask<Void, Void, Void>
+    {
+        protected void onPreExecute()
+        {
+            // running in the main thread
+            m_progressDialog = new ProgressDialog(TranslationDownloadActivity.this);
+            m_progressDialog.setCancelable(false);
+            m_progressDialog.setMessage(getText(R.string.text_downloading));
+            m_progressDialog.show();
+        }
+
+        protected Void doInBackground(Void... params)
+        {
+            // running in the worker thread
+            try {
+                URL url = new URL(BASE_URL + TRANSLATIONS_FILE);
+                HttpURLConnection httpConnection = (HttpURLConnection) url.openConnection();
+                InputStream in = new BufferedInputStream(httpConnection.getInputStream());
+                byte[] buffer = new byte[in.available()];
+                in.read(buffer);
+                in.close();
+
+                TranslationInfo[] installedTranslations = BibleReader.getInstance().installedTranslations();
+                int installedCount = installedTranslations.length;
+
+                JSONArray replyArray = new JSONArray(new String(buffer, "UTF8"));
+                final int length = replyArray.length();
+
+                TranslationInfo[] availableTranslations = new TranslationInfo[length];
+                int count = 0;
+                for (int i = 0; i < length; ++i) {
+                    JSONObject translationObject = replyArray.getJSONObject(i);
+                    String path = translationObject.getString("path");
+                    int j = 0;
+                    for (; j < installedCount; ++j) {
+                        if (installedTranslations[j].path.endsWith(path))
+                            break;
+                    }
+                    if (j < installedCount)
+                        continue;
+
+                    availableTranslations[count] = new TranslationInfo();
+                    availableTranslations[count].name = translationObject.getString("name");
+                    availableTranslations[count].path = path;
+                    availableTranslations[count].size = translationObject.getInt("size");
+                    ++count;
+                }
+
+                if (count > 0) {
+                    m_availableTranslations = new TranslationInfo[count];
+                    for (int i = 0; i < count; ++i)
+                        m_availableTranslations[i] = availableTranslations[i];
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        protected void onPostExecute(Void result)
+        {
+            // running in the main thread
+            if (m_availableTranslations == null || m_availableTranslations.length == 0) {
+                m_progressDialog.dismiss();
+                Toast.makeText(TranslationDownloadActivity.this, R.string.text_no_available_translation,
+                        Toast.LENGTH_SHORT).show();
+                finish();
+            } else {
+                int length = m_availableTranslations.length;
+                String[] texts = new String[length];
+                for (int i = 0; i < length; ++i)
+                    texts[i] = m_availableTranslations[i].name + " (" + m_availableTranslations[i].size + " KB)";
+                m_adapter.setTexts(texts);
+                m_progressDialog.dismiss();
+            }
+        }
+
+        private static final String TRANSLATIONS_FILE = "translations.json";
+
+        private ProgressDialog m_progressDialog;
+    }
+
+    private class TranslationDownloadAsyncTask extends AsyncTask<Integer, Integer, Void>
+    {
+        protected void onPreExecute()
+        {
+            // running in the main thread
+            m_progressDialog = new ProgressDialog(TranslationDownloadActivity.this);
+            m_progressDialog.setCancelable(false);
+            m_progressDialog.setMessage(getText(R.string.text_downloading));
+            m_progressDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+            m_progressDialog.setMax(100);
+            m_progressDialog.setProgress(0);
+            m_progressDialog.show();
+        }
+
+        protected Void doInBackground(Integer... positions)
+        {
+            // running in the worker thread
+            try {
+                TranslationInfo translationToDownload = m_availableTranslations[positions[0]];
+                String path = translationToDownload.path;
+
+                // creates sub-folder
+                File dir = new File(getFilesDir(), path);
+                dir.mkdir();
+
+                URL url = new URL(BASE_URL + path + ".zip");
+                HttpURLConnection httpConnection = (HttpURLConnection) url.openConnection();
+
+                ZipInputStream zis = new ZipInputStream(new BufferedInputStream(httpConnection.getInputStream()));
+                ZipEntry entry;
+                byte buffer[] = new byte[BUFFER_LENGTH];
+                int read = -1;
+                int downloaded = 0;
+                while ((entry = zis.getNextEntry()) != null) {
+                    // unzips and writes to internal storage
+                    FileOutputStream fos = new FileOutputStream(new File(dir, entry.getName()));
+                    BufferedOutputStream os = new BufferedOutputStream(fos, BUFFER_LENGTH);
+                    while ((read = zis.read(buffer, 0, BUFFER_LENGTH)) != -1)
+                        os.write(buffer, 0, read);
+                    os.flush();
+                    os.close();
+
+                    // notifies the progress
+                    publishProgress(++downloaded / 11);
+                }
+                zis.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        protected void onProgressUpdate(Integer... progress)
+        {
+            // running in the main thread
+            m_progressDialog.setProgress(progress[0]);
+        }
+
+        protected void onPostExecute(Void result)
+        {
+            // running in the main thread
+            BibleReader.getInstance().refresh();
+            m_progressDialog.dismiss();
+            finish();
+        }
+
+        private static final int BUFFER_LENGTH = 2048;
+
+        private ProgressDialog m_progressDialog;
+    }
+
+    private static final String BASE_URL = "http://bible.zionsoft.net/";
+
     private SelectionListAdapter m_adapter;
+    private TranslationInfo[] m_availableTranslations;
 }
