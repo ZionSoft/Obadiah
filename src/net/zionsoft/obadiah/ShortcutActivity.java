70becf5dc57136890cb4ddfa6f1a95429b3d0e3b
Xizhi Zhu <xizhi.zhu@gmail.com>
2012-08-11 16:24:41 +0300

    Added support for creating desktop shortcut.

---
+++ src/net/zionsoft/obadiah/ShortcutActivity.java
@@ -0,0 +1,22 @@
+package net.zionsoft.obadiah;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class ShortcutActivity extends Activity
+{
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+
+        Intent intent = new Intent();
+        intent.putExtra(Intent.EXTRA_SHORTCUT_INTENT, new Intent(this, BookSelectionActivity.class));
+        intent.putExtra(Intent.EXTRA_SHORTCUT_NAME, getString(R.string.app_name));
+        intent.putExtra(Intent.EXTRA_SHORTCUT_ICON_RESOURCE,
+                Intent.ShortcutIconResource.fromContext(this, R.drawable.ic_launcher));
+        setResult(RESULT_OK, intent);
+
+        finish();
+    }
+}
