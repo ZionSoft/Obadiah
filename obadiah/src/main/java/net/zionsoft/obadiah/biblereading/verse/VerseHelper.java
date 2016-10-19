d39aeabd5e08d25d891817d5c137178aef6e5d9d
Xizhi Zhu <xizhi.zhu@gmail.com>
2016-10-19 21:13:36 +0300

    Added position to book / chapter index.

---
+++ obadiah/src/main/java/net/zionsoft/obadiah/biblereading/verse/VerseHelper.java
@@ -0,0 +1,56 @@
+/*
+ * Obadiah - Simple and Easy-to-Use Bible Reader
+ * Copyright (C) 2016 ZionSoft
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package net.zionsoft.obadiah.biblereading.verse;
+
+import net.zionsoft.obadiah.model.domain.Bible;
+
+class VerseHelper {
+    static int positionToBookIndex(int position) throws IllegalArgumentException {
+        if (position < 0) {
+            throw new IllegalArgumentException("Invalid position: " + position);
+        }
+
+        final int bookCount = Bible.getBookCount();
+        for (int bookIndex = 0; bookIndex < bookCount; ++bookIndex) {
+            final int chapterCount = Bible.getChapterCount(bookIndex);
+            position -= chapterCount;
+            if (position < 0) {
+                return bookIndex;
+            }
+        }
+
+        throw new IllegalArgumentException("Invalid position: " + position);
+    }
+
+    static int positionToChapterIndex(int position) throws IllegalArgumentException {
+        if (position < 0) {
+            throw new IllegalArgumentException("Invalid position: " + position);
+        }
+
+        final int bookCount = Bible.getBookCount();
+        for (int bookIndex = 0; bookIndex < bookCount; ++bookIndex) {
+            final int chapterCount = Bible.getChapterCount(bookIndex);
+            if (position < chapterCount) {
+                return position;
+            }
+            position -= chapterCount;
+        }
+
+        throw new IllegalArgumentException("Invalid position: " + position);
+    }
+}
---
+++ obadiah/src/test/java/net/zionsoft/obadiah/biblereading/verse/VerseHelperTest.java
@@ -0,0 +1,45 @@
+/*
+ * Obadiah - Simple and Easy-to-Use Bible Reader
+ * Copyright (C) 2016 ZionSoft
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package net.zionsoft.obadiah.biblereading.verse;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class VerseHelperTest {
+    @Test
+    public void testPositionToBookIndex() {
+        Assert.assertEquals(0, VerseHelper.positionToBookIndex(0));
+        Assert.assertEquals(0, VerseHelper.positionToBookIndex(49));
+
+        Assert.assertEquals(1, VerseHelper.positionToBookIndex(50));
+        Assert.assertEquals(1, VerseHelper.positionToBookIndex(55));
+
+        Assert.assertEquals(65, VerseHelper.positionToBookIndex(1188));
+    }
+
+    @Test
+    public void testPositionToChapterIndex() {
+        Assert.assertEquals(0, VerseHelper.positionToChapterIndex(0));
+        Assert.assertEquals(49, VerseHelper.positionToChapterIndex(49));
+
+        Assert.assertEquals(0, VerseHelper.positionToChapterIndex(50));
+        Assert.assertEquals(5, VerseHelper.positionToChapterIndex(55));
+
+        Assert.assertEquals(21, VerseHelper.positionToChapterIndex(1188));
+    }
+}
