fcf1a0c74c297e8f96f7496f94e3e9d01b83d254
Xizhi Zhu <xizhi.zhu@gmail.com>
2016-12-19 22:21:15 +0200

    Fixed build break.

---
+++ obadiah/src/main/java/net/zionsoft/obadiah/utils/Triple.java
@@ -0,0 +1,56 @@
+/*
+ * Obadiah - Simple and Easy-to-Use Bible Reader
+ * Copyright (C) 2016 ZionSoft
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package net.zionsoft.obadiah.utils;
+
+public class Triple<F, S, T> {
+    public final F first;
+    public final S second;
+    public final T third;
+
+    public Triple(F first, S second, T third) {
+        this.first = first;
+        this.second = second;
+        this.third = third;
+    }
+
+    @Override
+    public int hashCode() {
+        return (first == null ? 0 : first.hashCode()) ^ (second == null ? 0 : second.hashCode())
+                ^ (third == null ? 0 : third.hashCode());
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof Triple)) {
+            return false;
+        }
+
+        final Triple<?, ?, ?> other = (Triple<?, ?, ?>) o;
+        return objectsEqual(other.first, first) && objectsEqual(other.second, second)
+                && objectsEqual(other.third, third);
+    }
+
+    private static boolean objectsEqual(Object a, Object b) {
+        return a == b || (a != null && a.equals(b));
+    }
+
+    @Override
+    public String toString() {
+        return "Triple{" + String.valueOf(first) + " " + String.valueOf(second) + " " + String.valueOf(third) + "}";
+    }
+}
